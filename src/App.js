e30b29ef94f291f4e5760fdab21a3ff4f9748cd5
mochochokoboiketlo <mochochokoboiketlo@gmail.com>
2023-08-21 08:32:46 +0200

    mas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">master
       <header className="App-header">
         <img src={`/images/good and evil.png`} className="App-logo" alt="logo" />
         
